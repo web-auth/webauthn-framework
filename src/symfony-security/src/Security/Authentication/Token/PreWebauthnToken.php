3282de59e2c7a778ce9c1a491f6988a519979b45
Spomky <florent@morselli.fr>
2019-02-08 10:19:41 +0100

    Test fixed

--- src/symfony-security/src/Security/Authentication/Token/PreWebauthnToken.php
+++ src/symfony-security/src/Security/Authentication/Token/PreWebauthnToken.php
@@ -63,7 +63,7 @@ class PreWebauthnToken extends AbstractToken
      */
     public function unserialize($serialized): void
     {
-        list($this->rememberMe, $this->providerKey, $parentStr) = unserialize($serialized, ['allowed_classes' => [self::class]]);
+        list($this->rememberMe, $this->providerKey, $parentStr) = unserialize($serialized);
 
         parent::unserialize($parentStr);
     }
