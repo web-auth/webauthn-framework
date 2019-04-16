4b57cf52ceb8f2ac682b1dc3bbcd224909609025
Spomky <florent@morselli.fr>
2019-04-16 18:15:42 +0200

    Symfony support

---
+++ src/symfony/src/Resources/config/metadata_service.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+use Webauthn\MetadataService\MetadataServiceCaller;
+
+return function (ContainerConfigurator $container) {
+    $container = $container->services()->defaults()
+        ->private()
+        ->autoconfigure()
+        ->autowire();
+
+    $container->set(MetadataServiceCaller::class)
+        ->public()
+        ->args([
+            ref('webauthn.metadata_service.http_client'),
+            ref('webauthn.metadata_service.request_factory'),
+            '%webauthn.metadata_service.token%',
+            ref('webauthn.metadata_service.caching_service')->nullOnInvalid(),
+        ]);
+};
