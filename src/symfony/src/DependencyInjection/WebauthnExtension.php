3e1113f8af7d0a3b3f34355a99c08f6f05e819d2
Spomky <florent@morselli.fr>
2019-04-18 12:53:43 +0200

    Bug fixed

--- src/symfony/src/DependencyInjection/WebauthnExtension.php
+++ src/symfony/src/DependencyInjection/WebauthnExtension.php
@@ -97,7 +97,7 @@ final class WebauthnExtension extends Extension implements PrependExtensionInter
             $container->setParameter('webauthn.android_safetynet.api_key', $config['android_safetynet']['api_key']);
             $loader->load('android_safetynet.php');
         }
-        if (true === $config['android_safetynet']['enabled']) {
+        if (true === $config['metadata_service']['enabled']) {
             $container->setAlias('webauthn.metadata_service.http_client', $config['metadata_service']['http_client']);
             $container->setAlias('webauthn.metadata_service.request_factory', $config['metadata_service']['request_factory']);
             $container->setParameter('webauthn.metadata_service.token', $config['metadata_service']['token']);
