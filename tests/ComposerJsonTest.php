37e3ea74c9ad645114bf61ea85e706e47e619477
Spomky <florent@morselli.fr>
2020-08-10 00:43:04 +0200

    Deps tests

--- tests/ComposerJsonTest.php
+++ tests/ComposerJsonTest.php
@@ -2,10 +2,24 @@
 
 declare(strict_types=1);
 
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2020 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 namespace Webauthn\Tests;
 
+use DirectoryIterator;
 use PHPUnit\Framework\TestCase;
+use Traversable;
 
+/**
+ * @internal
+ */
 class ComposerJsonTest extends TestCase
 {
     private const SRC_DIR = __DIR__.'/../src';
@@ -22,15 +36,15 @@ class ComposerJsonTest extends TestCase
             $packageDependencies = $this->getComposerDependencies(self::SRC_DIR.'/'.$package.'/composer.json');
             foreach ($packageDependencies as $dependency => $version) {
                 // Skip web-auth/* dependencies
-                if (0 === strpos($dependency, 'web-auth/')) {
+                if (0 === mb_strpos($dependency, 'web-auth/')) {
                     continue;
                 }
 
                 $message = sprintf('Dependency "%s" from package "%s" is not defined in root composer.json', $dependency, $package);
-                $this->assertArrayHasKey($dependency, $rootDependencies, $message);
+                static::assertArrayHasKey($dependency, $rootDependencies, $message);
 
                 $message = sprintf('Dependency "%s:%s" from package "%s" requires a different version in the root composer.json', $dependency, $version, $package);
-                $this->assertEquals($version, $rootDependencies[$dependency], $message);
+                static::assertEquals($version, $rootDependencies[$dependency], $message);
 
                 $usedDependencies[] = $dependency;
             }
@@ -38,7 +52,7 @@ class ComposerJsonTest extends TestCase
 
         $unusedDependencies = array_diff(array_keys($rootDependencies), array_unique($usedDependencies));
         $message = sprintf('Dependencies declared in root composer.json, which are not declared in any sub-package: %s', implode($unusedDependencies));
-        $this->assertCount(0, $unusedDependencies, $message);
+        static::assertCount(0, $unusedDependencies, $message);
     }
 
     /**
@@ -50,13 +64,13 @@ class ComposerJsonTest extends TestCase
         foreach ($this->listSubPackages() as $package) {
             $packageName = $this->getComposerPackageName(self::SRC_DIR.'/'.$package.'/composer.json');
             $message = sprintf('Root composer.json must replace the sub-packages "%s"', $packageName);
-            $this->assertArrayHasKey($packageName, $rootReplaces, $message);
+            static::assertArrayHasKey($packageName, $rootReplaces, $message);
         }
     }
 
-    private function listSubPackages(): \Traversable
+    private function listSubPackages(): Traversable
     {
-        foreach (new \DirectoryIterator(self::SRC_DIR) as $dirInfo) {
+        foreach (new DirectoryIterator(self::SRC_DIR) as $dirInfo) {
             if ($dirInfo->isDir() && !$dirInfo->isDot()) {
                 yield $dirInfo->getFilename();
             }
