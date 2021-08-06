8fbdb859c44ae518d07e5accf1297ce3f0bf5300
Spomky <contact@spomky-labs.com>
2021-08-06 17:18:59 +0200

    File header removed

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,6 @@
 <?php
 
-$header = 'The MIT License (MIT)
-
-Copyright (c) 2014-2020 Spomky-Labs
-
-This software may be modified and distributed under the terms
-of the MIT license.  See the LICENSE file for details.';
+$header = '';
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__.'/tests')
--- src/symfony/src/Resources/config/cose.php
+++ src/symfony/src/Resources/config/cose.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2014-2020 Spomky-Labs
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
-
 use Cose\Algorithm\Manager;
 use Cose\Algorithm\Signature\ECDSA\ES256;
 use Cose\Algorithm\Signature\ECDSA\ES256K;
