fe73c88f66ce1f971ff24680592fa052aad6cd43
Florent Morselli <contact@spomky-labs.com>
2022-09-01 20:43:06 +0200

    Fast tests

--- tests/symfony/functional/Attestation/AdditionalAuthenticatorTest.php
+++ tests/symfony/functional/Attestation/AdditionalAuthenticatorTest.php
@@ -161,14 +161,6 @@ final class AdditionalAuthenticatorTest extends WebTestCase
         static::assertSame('ok', $data['status']);
     }
 
-    /**
-     * @test
-     */
-    public function anExistingUserCanTestItsAuthenticators(): never
-    {
-        static::markTestSkipped('To be done');
-    }
-
     private function logIn(): void
     {
         $options = '{"status":"ok","errorMessage":"","rp":{"name":"Webauthn Demo","id":"webauthn.spomky-labs.com"},"pubKeyCredParams":[{"type":"public-key","alg":-8},{"type":"public-key","alg":-7},{"type":"public-key","alg":-43},{"type":"public-key","alg":-35},{"type":"public-key","alg":-36},{"type":"public-key","alg":-257},{"type":"public-key","alg":-258},{"type":"public-key","alg":-259},{"type":"public-key","alg":-37},{"type":"public-key","alg":-38},{"type":"public-key","alg":-39}],"challenge":"EhNVt3T8V12FJvSAc50nhKnZ-MEc-kf84xepDcGyN1g","attestation":"direct","user":{"name":"XY5nn3p_6olTLjoB2Jbb","id":"OTI5ZmJhMmYtMjM2MS00YmM2LWE5MTctYmI3NmFhMTRjN2Y5","displayName":"Bennie Moneypenny"},"authenticatorSelection":{"requireResidentKey":false,"userVerification":"preferred"},"timeout":60000}';
