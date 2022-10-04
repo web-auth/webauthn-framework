2d9cb0f8b2b5f553e65438ae3b09d36632194c01
Florent Morselli <contact@spomky-labs.com>
2022-10-04 15:12:23 +0200

    New FolderResourceMetadataService (#308)
    New FolderResourceMetadataService

---
+++ tests/library/Unit/Service/FolderResourceMetadataServiceTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webauthn\Tests\Unit\Service;
+
+use PHPUnit\Framework\TestCase;
+use Throwable;
+use Webauthn\MetadataService\Service\FolderResourceMetadataService;
+
+/**
+ * @internal
+ */
+final class FolderResourceMetadataServiceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function theListIsCorrect(): void
+    {
+        // Given
+        $service = new FolderResourceMetadataService(__DIR__ . '/../../mds////');
+
+        // When
+        $list = [...$service->list()];
+
+        // Then
+        static::assertContains('9debdbfd-14dd-4e8d-877b-000000000000', $list);
+        static::assertContains('9debdbfd-14dd-4e8d-877b-4a6e35ddb375', $list);
+        static::assertContains('91dfead7-959e-4475-ad26-9b0d482be089', $list);
+        static::assertContains('a8d59924-63b7-49ea-b9de-34a753de1e01', $list);
+    }
+
+    /**
+     * @test
+     */
+    public function theAAGUIDIsSupported(): void
+    {
+        // Given
+        $service = new FolderResourceMetadataService(__DIR__ . '/../../mds');
+
+        // When
+        $isValid = $service->has('9debdbfd-14dd-4e8d-877b-000000000000');
+
+        // Then
+        static::assertTrue($isValid);
+    }
+
+    /**
+     * @test
+     */
+    public function theMetadataStatementIsRead(): void
+    {
+        // Given
+        $service = new FolderResourceMetadataService(__DIR__ . '/../../mds');
+
+        // When
+        $mds = $service->get('9debdbfd-14dd-4e8d-877b-4a6e35ddb375');
+
+        // Then
+        static::assertSame('9debdbfd-14dd-4e8d-877b-4a6e35ddb375', $mds->getAaguid());
+    }
+
+    /**
+     * @test
+     */
+    public function theMetadataStatementIsInvalid(): void
+    {
+        // Then
+        static::expectException(Throwable::class);
+        static::expectExceptionMessage('Syntax error');
+
+        // Given
+        $service = new FolderResourceMetadataService(__DIR__ . '/../../mds');
+
+        // When
+        $service->get('9debdbfd-14dd-4e8d-877b-000000000000');
+    }
+}
