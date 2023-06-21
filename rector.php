86647fdd5043c456c3480b9bb3b44a65943e9bda
Florent Morselli <contact@spomky-labs.com>
2023-06-21 11:15:48 +0200

    BE and BS flags support (#424)
    BE and BS flags support

--- rector.php
+++ rector.php
@@ -6,6 +6,8 @@ use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
 use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\PHPUnit\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
@@ -23,19 +25,25 @@ return static function (RectorConfig $config): void {
     $config->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
     $config->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
     $config->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $config->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
     $config->import(PHPUnitSetList::PHPUNIT_EXCEPTION);
+    $config->import(PHPUnitSetList::REMOVE_MOCKS);
     $config->import(PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD);
+    $config->import(PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES);
     $config->import(PHPUnitLevelSetList::UP_TO_PHPUNIT_100);
-    $config->import(PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER);
     $config->paths([__DIR__ . '/src', __DIR__ . '/tests']);
     $config->skip([
-        'src/symfony/src/DependencyInjection/Configuration.php',
-        'src/symfony/src/Routing/Loader.php',
-        'tests/symfony/config/routing.php',
+        __DIR__ . '/src/symfony/src/DependencyInjection/Configuration.php',
+        __DIR__ . '/src/symfony/src/Routing/Loader.php',
+        __DIR__ . '/tests/symfony/config/routing.php',
         RemoveUnusedPrivateMethodParameterRector::class => [
-            __DIR__ . '*/DependencyInjection/Configuration.php',
+            __DIR__ . '/src/symfony/src/DependencyInjection/Configuration.php',
+        ],
+        ReadOnlyPropertyRector::class => [
+            __DIR__ . '/src/metadata-service/src/Statement/MetadataStatement.php',
         ],
     ]);
+    $config->services()->remove(PreferPHPUnitThisCallRector::class);
     $config->phpVersion(PhpVersion::PHP_81);
     $config->parallel();
     $config->importNames();
