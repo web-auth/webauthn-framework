b001705d8e8c064c3ab4c31bec1dab7e24bf3ea0
Florent Morselli <contact@spomky-labs.com>
2024-03-22 21:51:36 +0100

    Remove unnecessary denormalizer exception checks and deprecated funct… (#577)
    * Remove unnecessary denormalizer exception checks and deprecated functions
    
    This commit removes unnecessary checks for denormalizer nullity and throws subsequent exceptions. The checks were removed in various Denormalizer files, resulting in a cleaner codebase.
    
    * Update phpstan-baseline.neon file
    
    The phpstan-baseline.neon file has been updated to reflect recent changes. Messages that referenced a strict comparison between a DenormalizerInterface and null have been removed as these were always evaluating to false. Additionally, a new message has been added in the path of FidoAllianceCompliantMetadataService.
    
    * Refactor AuthenticationExtensionsDenormalizer and update type hints
    
    This commit refactors the 'denormalize' method in the AuthenticationExtensionsDenormalizer class, ensuring that all data passed to the method is an array by using a new assert statement. Changes have also been made to the AuthenticationExtensions class, updating type hints arrays. These improvements in type validation have led to the removal of certain error messages from the phpstan-baseline.neon file.

--- src/webauthn/src/Denormalizer/CollectedClientDataDenormalizer.php
+++ src/webauthn/src/Denormalizer/CollectedClientDataDenormalizer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Webauthn\Denormalizer;
 
-use Symfony\Component\Serializer\Exception\BadMethodCallException;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
@@ -17,10 +16,6 @@ final class CollectedClientDataDenormalizer implements DenormalizerInterface, De
 
     public function denormalize(mixed $data, string $type, string $format = null, array $context = []): mixed
     {
-        if ($this->denormalizer === null) {
-            throw new BadMethodCallException('Please set a denormalizer before calling denormalize()!');
-        }
-
         return CollectedClientData::create($data, json_decode($data, true, flags: JSON_THROW_ON_ERROR));
     }
 
