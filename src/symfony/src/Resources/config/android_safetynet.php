be5d8a3bb0b92a383ee1f8e550095952448f026a
Florent Morselli <florent.morselli@spomky-labs.com>
2024-04-09 09:57:53 +0200

    Update deprecation message for Android SafetyNet
    Deprecation message for Android SafetyNet has been reformatted for better readability. Warnings about use of deprecated methods related to Android SafetyNet are also added in the phpstan-baseline.neon file. This sends clearer deprecation alerts to developers using the deprecated class across various files.

--- src/symfony/src/Resources/config/android_safetynet.php
+++ src/symfony/src/Resources/config/android_safetynet.php
@@ -20,7 +20,11 @@ return static function (ContainerConfigurator $container): void {
     if (class_exists(JWKFactory::class) && class_exists(RS256::class)) {
         $container
             ->set(AndroidSafetyNetAttestationStatementSupport::class)
-            ->deprecate('web-auth/webauthn-symfony-bundle','4.9.0', 'The "%service_id%" service is deprecated and will be removed in version 5.0.0. Android SafetyNet is now deprecated.')
+            ->deprecate(
+                'web-auth/webauthn-symfony-bundle',
+                '4.9.0',
+                'The "%service_id%" service is deprecated and will be removed in version 5.0.0. Android SafetyNet is now deprecated.'
+            )
             ->args([service(ClockInterface::class)->nullOnInvalid()])
             ->call('setMaxAge', [param('webauthn.android_safetynet.max_age')])
             ->call('setLeeway', [param('webauthn.android_safetynet.leeway')]);
