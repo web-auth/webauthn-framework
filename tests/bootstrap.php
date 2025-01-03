1ecaff4b6add0c52f576201a66c51d012b483c12
Florent Morselli <florent.morselli@spomky-labs.com>
2025-01-03 19:42:36 +0100

    Refactor test configuration and improve error handling.
    Updated PHPUnit environment settings to adjust deprecations handling and removed outdated entries in phpstan-baseline. Additionally, added a Symfony error handler in the test bootstrap for better exception management.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,13 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
+set_exception_handler([new ErrorHandler(), 'handleException']);
 if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
-    require_once dirname(__DIR__) . '/config/bootstrap.php';
+    require dirname(__DIR__) . '/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
