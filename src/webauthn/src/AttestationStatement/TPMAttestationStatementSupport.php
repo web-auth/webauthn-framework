6f23dcb9f342fe525044d91940938e3c7863414d
Florent Morselli <florent.morselli@spomky-labs.com>
2025-10-22 17:41:38 +0200

    chore: simplify exception throwing for extendedKeyUsage validation

--- src/webauthn/src/AttestationStatement/TPMAttestationStatementSupport.php
+++ src/webauthn/src/AttestationStatement/TPMAttestationStatementSupport.php
@@ -411,9 +411,7 @@ final class TPMAttestationStatementSupport implements AttestationStatementSuppor
             $parsed['extensions']['extendedKeyUsage'],
             ['2.23.133.8.3', 'Attestation Identity Key Certificate'],
             true
-        ) || throw AttestationStatementVerificationException::create(
-            'The "extendedKeyUsage" is invalid'
-        );
+        ) || throw AttestationStatementVerificationException::create('The "extendedKeyUsage" is invalid');
 
         // id-fido-gen-ce-aaguid OID check
         in_array('1.3.6.1.4.1.45724.1.1.4', $parsed['extensions'], true) && ! hash_equals(
