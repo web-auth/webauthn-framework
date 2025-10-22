a0794b7d2f2cab5da05b7ac6e1a27176271852e8
Joost de Bruijn <joostdebruijn@users.noreply.github.com>
2025-10-22 10:28:57 +0200

    feat: add webauthn signal api serializers (#720)
    * Add signal classes and denormalizers for WebAuthn events
    
    Introduced `Signal` interface along with three implementations: `AllAcceptedCredentials`, `CurrentUserDetails`, and `UnknownCredential`. Added corresponding denormalizers to handle serialization and deserialization of these events, updating `WebauthnSerializerFactory` to register them. Extended `SerializerTest` to validate proper functionality with new test cases.
    
    * Update PHPStan baseline with new rules for Signal denormalizers
    
    Expanded the PHPStan baseline to include new warnings related to type handling and offset accessibility in Signal denormalizers (`SignalAllAcceptedCredentialsDenormalizer`, `SignalCurrentUserDetailsDenormalizer`, and `SignalUnknownCredentialDenormalizer`), ensuring accurate tracking of static analysis issues.
    
    ---------
    
    Co-authored-by: Florent Morselli <florent.morselli@spomky-labs.com>

---
+++ src/webauthn/src/Denormalizer/SignalUnknownCredentialDenormalizer.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webauthn\Denormalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Webauthn\Signal\UnknownCredential;
+use function assert;
+
+class SignalUnknownCredentialDenormalizer implements NormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    /**
+     * @return array<class-string, bool>
+     */
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            UnknownCredential::class => true,
+        ];
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function normalize(mixed $data, ?string $format = null, array $context = []): array
+    {
+        assert($data instanceof UnknownCredential);
+
+        $normalized_rp = $this->normalizer->normalize($data->rp, $format, $context);
+
+        $normalized_credential = $this->normalizer->normalize($data->credential, $format, $context);
+
+        return [
+            'rpId' => $normalized_rp['id'],
+            'credentialId' => $normalized_credential['id'],
+        ];
+    }
+
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
+    {
+        return $data instanceof UnknownCredential;
+    }
+}
