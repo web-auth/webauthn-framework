a96f59c633ba1fc2ea24a653377e38de315fa4b6
Florent Morselli <florent.morselli@spomky-labs.com>
2025-10-22 16:10:20 +0200

    chore: update deptrac configuration paths and add ObjectNormalizer to serializer factory

--- src/webauthn/src/Denormalizer/WebauthnSerializerFactory.php
+++ src/webauthn/src/Denormalizer/WebauthnSerializerFactory.php
@@ -11,6 +11,7 @@ use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\UidNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
