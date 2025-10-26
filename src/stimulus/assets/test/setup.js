06acfd24639f1c20a201d6d572269b5be594d8f6
Florent Morselli <florent.morselli@spomky-labs.com>
2025-10-26 14:04:01 +0100

    feat: enhance WebAuthn authentication and registration tests with detailed event handling and error management

--- src/stimulus/assets/test/setup.js
+++ src/stimulus/assets/test/setup.js
@@ -1,3 +1,20 @@
 'use strict';
 
 import '@symfony/stimulus-testing/setup';
+
+// Polyfill for jsdom's missing requestSubmit
+if (typeof HTMLFormElement.prototype.requestSubmit === 'undefined') {
+    HTMLFormElement.prototype.requestSubmit = function (submitter) {
+        if (submitter) {
+            submitter.click();
+            return;
+        }
+
+        const submitEvent = new Event('submit', {
+            bubbles: true,
+            cancelable: true,
+        });
+
+        this.dispatchEvent(submitEvent);
+    };
+}
