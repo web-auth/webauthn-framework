e24ce9d56c9da734e5064b90d79ef440328e6b25
Florent Morselli <florent.morselli@spomky-labs.com>
2025-10-26 10:13:30 +0100

    feat: update Babel and Jest configurations to remove TypeScript support and adjust build scripts

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
     presets: [
         ['@babel/preset-env', {targets: {node: 'current'}}],
-        '@babel/react',
-        ['@babel/preset-typescript', { allowDeclareFields: true }]
+        '@babel/react'
     ],
 };
