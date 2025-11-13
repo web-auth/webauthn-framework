cdfa61563ed664c819440a1df31cffa714a1410b
Florent Morselli <florent.morselli@spomky-labs.com>
2025-11-13 22:32:18 +0100

    feat: initialize NPM package structure and add publishing workflow

---
+++ src/stimulus/assets/src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @web-auth/webauthn-stimulus
+ * WebAuthn Stimulus Controllers
+ */
+
+export { default as AuthenticationController } from './authentication-controller.js';
+export { default as RegistrationController } from './registration-controller.js';
+export { default as WebauthnController } from './controller.js';
+export { default as BaseController } from './base-controller.js';
