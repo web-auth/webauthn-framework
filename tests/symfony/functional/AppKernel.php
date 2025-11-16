f134fec6e99aabf1470cbb426aa206707bd5f20b
Florent Morselli <contact@spomky-labs.com>
2025-11-16 23:03:11 +0100

    feat: update doctrine bundle configuration and dependencies for compatibility (#766)

--- tests/symfony/functional/AppKernel.php
+++ tests/symfony/functional/AppKernel.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Webauthn\Tests\Bundle\Functional;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
@@ -38,5 +40,22 @@ final class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__ . '/../config/config.yml');
+
+        $loader->load(function ($container) {
+            $isDoctrineBundle2 = InstalledVersions::satisfies(
+                new VersionParser(),
+                'doctrine/doctrine-bundle',
+                '^2.0'
+            );
+
+            if ($isDoctrineBundle2) {
+                $container->loadFromExtension('doctrine', [
+                    'orm' => [
+                        'enable_lazy_ghost_objects' => true,
+                        'auto_generate_proxy_classes' => true,
+                    ],
+                ]);
+            }
+        });
     }
 }
