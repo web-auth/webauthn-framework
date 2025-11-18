32816d3cc16577b03077adf18d108ce87cc07dda
Florent Morselli <florent.morselli@spomky-labs.com>
2025-11-18 20:23:58 +0100

    fix: update asset imports and remove unused asset mapper configuration

--- src/stimulus/src/WebauthnStimulusBundle.php
+++ src/stimulus/src/WebauthnStimulusBundle.php
@@ -4,40 +4,8 @@ declare(strict_types=1);
 
 namespace Webauthn\Stimulus;
 
-use Symfony\Component\AssetMapper\AssetMapperInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class WebauthnStimulusBundle extends AbstractBundle
 {
-    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        if (! $this->isAssetMapperAvailable($builder)) {
-            return;
-        }
-
-        $builder->prependExtensionConfig('framework', [
-            'asset_mapper' => [
-                'paths' => [
-                    dirname(__DIR__) . '/assets/src' => '@web-auth/webauthn-stimulus',
-                ],
-            ],
-        ]);
-    }
-
-    private function isAssetMapperAvailable(ContainerBuilder $container): bool
-    {
-        if (! interface_exists(AssetMapperInterface::class)) {
-            return false;
-        }
-
-        // check that FrameworkBundle is installed
-        $bundlesMetadata = $container->getParameter('kernel.bundles_metadata');
-        if (! isset($bundlesMetadata['FrameworkBundle'])) {
-            return false;
-        }
-
-        return is_file($bundlesMetadata['FrameworkBundle']['path'] . '/Resources/config/asset_mapper.php');
-    }
 }
