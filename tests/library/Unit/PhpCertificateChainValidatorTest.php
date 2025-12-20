49bbf603b9c81ab612b1250135423f9ef895b513
Florent Morselli <florent.morselli@spomky-labs.com>
2025-12-20 11:52:01 +0100

    test: refactor PhpCertificateChainValidatorTest for improved readability and consistency

--- tests/library/Unit/PhpCertificateChainValidatorTest.php
+++ tests/library/Unit/PhpCertificateChainValidatorTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
+use Webauthn\Exception\CertificateChainException;
 use Webauthn\MetadataService\CertificateChain\PhpCertificateChainValidator;
 
 /**
@@ -22,22 +23,17 @@ final class PhpCertificateChainValidatorTest extends TestCase
         $intermediateCert = file_get_contents(__DIR__ . '/../certificates/intermediate-ca/intermediate-ca.pem');
         $rootCert = file_get_contents(__DIR__ . '/../certificates/intermediate-ca/root-ca.pem');
 
-        $httpClient = new MockHttpClient([
-            new MockResponse(''),
-        ]);
+        $httpClient = new MockHttpClient([new MockResponse('')]);
 
         $validator = PhpCertificateChainValidator::create($httpClient);
 
         // Test with root CA as trust anchor (self-signed certificate)
         // The untrusted chain includes leaf and intermediate
         // This should work with both old and new implementation
-        $validator->check(
-            [$leafCert, $intermediateCert],
-            [$rootCert]
-        );
+        $validator->check([$leafCert, $intermediateCert], [$rootCert]);
 
         // If we reach here, validation succeeded
-        $this->assertTrue(true);
+        static::assertTrue(true);
     }
 
     #[Test]
@@ -45,22 +41,17 @@ final class PhpCertificateChainValidatorTest extends TestCase
     {
         $leafCert = file_get_contents(__DIR__ . '/../certificates/intermediate-ca/leaf.pem');
 
-        $httpClient = new MockHttpClient([
-            new MockResponse(''),
-        ]);
+        $httpClient = new MockHttpClient([new MockResponse('')]);
 
         $validator = PhpCertificateChainValidator::create($httpClient);
 
         // When the leaf certificate is the same as the trust anchor, it should be accepted
         // This is a valid case mentioned in FIDO MDS spec:
         // "A trust anchor can be [...] even the attestation certificate itself"
-        $validator->check(
-            [$leafCert],
-            [$leafCert]
-        );
+        $validator->check([$leafCert], [$leafCert]);
 
         // If we reach here, validation succeeded
-        $this->assertTrue(true);
+        static::assertTrue(true);
     }
 
     #[Test]
@@ -69,17 +60,12 @@ final class PhpCertificateChainValidatorTest extends TestCase
         $leafCert = file_get_contents(__DIR__ . '/../certificates/intermediate-ca/leaf.pem');
         $rootCert = file_get_contents(__DIR__ . '/../certificates/intermediate-ca/root-ca.pem');
 
-        $httpClient = new MockHttpClient([
-            new MockResponse(''),
-        ]);
+        $httpClient = new MockHttpClient([new MockResponse('')]);
 
         $validator = PhpCertificateChainValidator::create($httpClient);
 
         // Leaf certificate cannot be verified with root CA directly (missing intermediate)
-        $this->expectException(\Webauthn\Exception\CertificateChainException::class);
-        $validator->check(
-            [$leafCert],
-            [$rootCert]
-        );
+        $this->expectException(CertificateChainException::class);
+        $validator->check([$leafCert], [$rootCert]);
     }
 }
