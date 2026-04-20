e51a4b1e5a77078990be8545ff9042d6b9adaf4f
Sam Reed <sam@reedyboy.net>
2026-04-20 15:51:57 +0100

    CheckUserHandle: Use hash_equals for comparing user handles

--- src/webauthn/src/CeremonyStep/CheckUserHandle.php
+++ src/webauthn/src/CeremonyStep/CheckUserHandle.php
@@ -36,12 +36,12 @@ final class CheckUserHandle implements CeremonyStep
         $credentialUserHandle = $credentialRecord->userHandle;
         $responseUserHandle = $authenticatorResponse->userHandle;
         if ($userHandle !== null) { //If the user was identified before the authentication ceremony was initiated,
-            $credentialUserHandle === $userHandle || throw InvalidUserHandleException::create();
+            hash_equals($credentialUserHandle, $userHandle) || throw InvalidUserHandleException::create();
             if ($responseUserHandle !== null && $responseUserHandle !== '') {
-                $credentialUserHandle === $responseUserHandle || throw InvalidUserHandleException::create();
+                hash_equals($credentialUserHandle, $responseUserHandle) || throw InvalidUserHandleException::create();
             }
         } else {
-            ($responseUserHandle !== '' && $credentialUserHandle === $responseUserHandle) || throw InvalidUserHandleException::create();
+            ($responseUserHandle !== '' && hash_equals($credentialUserHandle, $responseUserHandle)) || throw InvalidUserHandleException::create();
         }
     }
 }
